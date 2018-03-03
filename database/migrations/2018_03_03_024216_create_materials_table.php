4bcec8fb7eb256e1f12f3bd9bfc22c6801d5085d
zhouqihang <console_log@126.com>
2018-03-03 10:51:52 +0800

    :sparkles: 添加materials数据迁移文件

---
+++ database/migrations/2018_03_03_024216_create_materials_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMaterialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('material_number')->default('');
+            $table->string('material_title')->default('');
+            $table->string('material_unit');
+            $table->integer('material_danger')->default(0);
+            $table->integer('material_count')->default(0);
+            $table->string('material_remark')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materials');
+    }
+}
