25b7e411a80fe552bd9b1808c3aa9d13ebb8734c
zhouqihang <console_log@126.com>
2018-03-05 06:55:28 +0000

    :sparkles: 添加products数据迁移文件

---
+++ database/migrations/2018_03_05_145324_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('prod_number')->default('')->unique()->nullable();
+            $table->string('prod_title')->default('')->unique()->nullable();
+            $table->string('prod_unit');
+            $table->integer('prod_count')->default(0)->unsigned();
+            $table->string('prod_remark')->default('')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
